1d31062e12713cfe7140b737b0ced3e869b27688
Kava Developers <mehul9921@gmail.com>
2020-12-18 00:30:49 +0530

    Update ongoing.php

--- application/views/orders/ongoing.php
+++ application/views/orders/ongoing.php
@@ -71,7 +71,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Assign Service Provider</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Assign Driver</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
