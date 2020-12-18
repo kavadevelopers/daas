e6fee15b8d572b6b1c5387ded1aa69d473451357
Kava Developers <mehul9921@gmail.com>
2020-12-18 23:04:37 +0530

    color

--- application/views/orders/new.php
+++ application/views/orders/new.php
@@ -45,7 +45,7 @@
                                         <a href="<?= base_url('orders/view/').$value['id'] ?>/new" class="btn btn-success btn-mini" title="View">
                                             <i class="fa fa-eye"></i>
                                         </a>
-                                        <a href="<?= base_url('orders/complete/').$value['id'] ?>/new" class="btn btn-success btn-mini" title="Complete Order" onclick="return confirm('Are you sure want to Complete Order ?')">
+                                        <a href="<?= base_url('orders/complete/').$value['id'] ?>/new" class="btn btn-info btn-mini" title="Complete Order" onclick="return confirm('Are you sure want to Complete Order ?')">
                                             <i class="fa fa-check"></i>
                                         </a>
                                         <a href="<?= base_url('orders/cancel/').$value['id'] ?>/new" class="btn btn-warning btn-mini" title="Cancel Order" onclick="return confirm('Are you sure want to Cancel Order ?')">
