1edd7058c95713bfca83b7a996dba4cac5c44ea0
Kava Developers <mehul9921@gmail.com>
2020-12-26 14:56:38 +0530

    Update view.php

--- application/views/orders/view.php
+++ application/views/orders/view.php
@@ -106,6 +106,7 @@
 			                            </div>
 			                        </div>
 			                    </div>
+			                    <h6 class="m-b-20 p-b-5 b-b-default f-w-600"></h6>
 			                    <div class="row">
 			                        <div class="col-sm-12">
 			                            <p class="m-b-10 f-w-600">Rating Description</p>
