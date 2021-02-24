ee4459205ecc00b40d26ed9b081a7fd4dcf7de02
Kava Developers <mehul9921@gmail.com>
2021-02-24 19:09:06 +0530

    Update CronController.php

--- application/controllers/CronController.php
+++ application/controllers/CronController.php
@@ -8,23 +8,25 @@ class CronController extends CI_Controller
 
 	public function index()
 	{
-		// $this->db->insert('cronchk',['time' => date('Y-m-d H:i:s')]);
-		// exit;
+// 		$this->db->insert('cronchk',['time' => date('Y-m-d H:i:s')]);
+// 		exit;
 		$users = $this->db->get_where('z_customer',['df' => '','free' => ''])->result_array();
 		foreach ($users as $key => $user) {
-			if(daysBeetweenDates($user->sub_expired_on) <= 0){
+		    
+			if(daysBeetweenDates($user['sub_expired_on']) >= 0){
 				//Expired Notification
+				
 				sendPush(
-					[$user->token],
+					[$user['token']],
 					"Subscription",
 					"Your subscription plan has been expired. To enjoy unlimited deliveries and services subscribe now!"
 				);
 			}
 
-			if(daysBeetweenDates($user->sub_expired_on) == 2 || daysBeetweenDates($user->sub_expired_on) == 2){
+			if(daysBeetweenDates($user['sub_expired_on']) == -2 || daysBeetweenDates($user['sub_expired_on']) == -1){
 				// Two Days remaining notification
 				sendPush(
-					[$user->token],
+					[$user['token']],
 					"Subscription",
 					"Your Subscription is about to end soon. To enjoy uninterrupted and unlimited deliveries renew your subscription on time!"
 				);
